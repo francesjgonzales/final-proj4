89f07548f06873185b5a7f9e3c7b110e31f4e7c0
francesjgonzales <francesj7837@gmail.com>
2020-12-07 15:07:21 +0000

    added shoe categories in model

--- shoes/models.py
+++ shoes/models.py
@@ -3,23 +3,23 @@ from django.db import models
 
 # Create your models here.
 class Shoe(models.Model):
-    BOOL_OPTIONS = (
-        (True, 'Available'),
-        (False, 'Sold Out')
+    SHOE_AVAILABILITY = (
+        ('Available', 'Available'),
+        ('Sold-out', 'Sold Out')
     )
 
-    SHOE_AVAILABILITY = (
-        ('available', 'Available'),
-        ('sold-out', 'Sold Out')
+    CATEGORIES = (
+        ('New-Release', 'NEW RELEASE'),
+        ('Popular', 'POPULAR')
     )
 
     brand = models.CharField(blank=False, max_length=255)
     price = models.IntegerField(blank=False)
     color = models.CharField(blank=False, max_length=155)
-    size = models.DecimalField(max_digits=5, decimal_places=2)
-    soldOut = models.BooleanField(choices=BOOL_OPTIONS)
+    size = models.DecimalField(max_digits=5, decimal_places=1)
     shoeAvail = models.CharField(max_length=9, choices=SHOE_AVAILABILITY)
+    shoeCategories = models.CharField(max_length=11, choices=CATEGORIES)
 
     def __str__(self):
-        return self.brand + "" + str(self.price) + self.color 
-        + str(self.size) + str(self.soldOut) + str(self.shoeAvail)
+        return self.brand + "" + str(self.price) + self.color
+        + str(self.size) + str(self.shoeAvail) + str(self.shoeCategories)
