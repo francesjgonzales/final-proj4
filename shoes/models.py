fdac6f5f4ebb77ad2477909eb87ccbd92a1777b1
francesjgonzales <francesj7837@gmail.com>
2020-12-08 15:47:34 +0000

    added newShoe model

--- shoes/models.py
+++ shoes/models.py
@@ -1,25 +1,39 @@
 from django.db import models
+from datetime import date, datetime
 
 
 # Create your models here.
 class Shoe(models.Model):
     SHOE_AVAILABILITY = (
-        ('Available', 'Available'),
-        ('Sold-out', 'Sold Out')
+        ('In-stock', 'In Stock'),
+        ('Sold-out', 'Sold Out'),
+        ('Upcoming', 'Upcoming')
     )
 
-    CATEGORIES = (
-        ('New-Release', 'NEW RELEASE'),
-        ('Popular', 'POPULAR')
-    )
-
-    brand = models.CharField(blank=False, max_length=255)
+    shoeModel = models.CharField(blank=False, max_length=255)
+    shoeBrand = models.CharField(blank=False, max_length=255)
     price = models.IntegerField(blank=False)
     color = models.CharField(blank=False, max_length=155)
     size = models.DecimalField(max_digits=5, decimal_places=1)
     shoeAvail = models.CharField(max_length=9, choices=SHOE_AVAILABILITY)
-    shoeCategories = models.CharField(max_length=11, choices=CATEGORIES)
 
     def __str__(self):
-        return self.brand + "" + str(self.price) + self.color
-        + str(self.size) + str(self.shoeAvail) + str(self.shoeCategories)
+        return self.shoeModel + "" + self.shoeBrand
+
+
+class NewShoe(models.Model):
+    SHOE_AVAILABILITY = (
+        ('In-stock', 'In Stock'),
+        ('Sold-out', 'Sold Out'),
+        ('Coming-Soon', 'Coming Soon')
+    )
+
+    shoeModel = models.CharField(blank=False, max_length=255)
+    shoeBrand = models.CharField(blank=False, max_length=255)
+    price = models.IntegerField(blank=False)
+    color = models.CharField(blank=False, max_length=155)
+    releaseDate = models.DateTimeField(default=datetime.now, blank=True)
+    shoeAvail = models.CharField(max_length=11, choices=SHOE_AVAILABILITY)
+
+    def __str__(self):
+        return self.shoeModel
--- shoes/views.py
+++ shoes/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, HttpResponse
-from .models import Shoe
+from .models import Shoe, NewShoe
 
 # Create your views here.
 
 
 def index(request):
     shoes = Shoe.objects.all()
+    newShoes = NewShoe.objects.all()
     return render(request, 'shoes/index.template.html', {
-        'shoes': shoes
+        'shoes': shoes,
+        'newShoes': newShoes
     })
