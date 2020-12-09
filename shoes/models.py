4ce46ce4d53d4c89f06f32f5558aee5f1b096e1a
francesjgonzales <francesj7837@gmail.com>
2020-12-09 14:43:29 +0000

    added multiselectfield in shoe size

--- shoes/models.py
+++ shoes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from datetime import date, datetime
+from datetime import datetime
+from multiselectfield import MultiSelectField
 
 
 # Create your models here.
@@ -10,11 +11,27 @@ class Shoe(models.Model):
         ('Upcoming', 'Upcoming')
     )
 
+    SHOE_SIZES = (
+        ('5', '5'),
+        ('5.5', '5.5'),
+        ('6', '6'),
+        ('6.5', '6.5'),
+        ('7', '7'),
+        ('7.5', '7.5'),
+        ('8', '8'),
+        ('8.5', '8.5'),
+        ('9', '9'),
+        ('9.5', '9.5'),
+        ('10', '10'),
+        ('10.5', '10.5'),
+        ('11', '11'),
+    )
+
     shoeModel = models.CharField(blank=False, max_length=255)
     shoeBrand = models.CharField(blank=False, max_length=255)
     price = models.IntegerField(blank=False)
     color = models.CharField(blank=False, max_length=155)
-    size = models.DecimalField(max_digits=5, decimal_places=1)
+    size = MultiSelectField(choices=SHOE_SIZES)
     shoeAvail = models.CharField(max_length=9, choices=SHOE_AVAILABILITY)
 
     def __str__(self):
