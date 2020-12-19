970a243b419bbfc20b80e161a175f2b6f30beeec
francesjgonzales <francesj7837@gmail.com>
2020-12-19 04:21:08 +0000

    added master template and override templates

--- TheNewSoleProject/settings.py
+++ TheNewSoleProject/settings.py
@@ -66,7 +66,8 @@ ROOT_URLCONF = 'TheNewSoleProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'templates', 'allauth')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
