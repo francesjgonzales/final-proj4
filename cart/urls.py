79a3b57d212078b03b915baae46c207649e3a37a
francesjgonzales <francesj7837@gmail.com>
2020-12-24 09:21:04 +0000

    fixed cart

--- cart/urls.py
+++ cart/urls.py
@@ -4,5 +4,6 @@ import cart.views
 urlpatterns = [
     path('add/<shoe_id>', cart.views.add_to_cart, name='add_to_cart'),
     path('view', cart.views.view_cart, name='view_cart'),
+    path('remove/<shoe_id>', cart.views.remove_from_cart,                               name='remove_from_cart')
 
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
 from shoes.models import Shoe
 
 # Create your views here.
@@ -6,19 +7,25 @@ from shoes.models import Shoe
 
 def add_to_cart(request, shoe_id):
     shoe = get_object_or_404(Shoe, pk=shoe_id)
+
     cart = request.session.get('shopping_cart', {})
 
     if shoe_id not in cart:
         cart[shoe_id] = {
             'id': shoe_id,
             'shoeModel': shoe.shoeModel,
-            'price': 99,
-            'qty': 1
+            'price': str(shoe.price),
+            'total_price': float(shoe.price),
+            'qty': 1,
+            'shoe_size': shoe.shoe_size,
+            'image': str(shoe.image)
         }
     else:
         cart[shoe_id]['qty'] += 1
+        cart[shoe_id]['total_price'] = round(int(cart[shoe_id]['qty']) * float(cart[shoe_id]['price']), 2)
 
     request.session['shopping_cart'] = cart
+    messages.success(request, 'Item is added to your cart')
     return redirect(reverse('view_cart'))
 
 
@@ -27,3 +34,15 @@ def view_cart(request):
     return render(request, 'cart/view_cart.template.html', {
         'cart': cart
     })
+
+
+def remove_from_cart(request, shoe_id):
+    cart = request.session.get('shopping_cart', {})
+
+    if shoe_id in cart:
+        del cart[shoe_id]
+
+        request.session['shopping_cart'] = cart
+        messages.info(request, 'Item is removed from your cart')
+
+    return redirect(reverse('main_shoe'))
