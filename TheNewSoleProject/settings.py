c7a4547d4c2275ebe7ae7bd1314ebac4ecb03ad6
francesjgonzales <francesj7837@gmail.com>
2020-12-26 17:48:31 +0000

    added email settings

--- TheNewSoleProject/settings.py
+++ TheNewSoleProject/settings.py
@@ -102,7 +102,18 @@ ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/success"
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+TEST_EMAIL = os.environ.get('TEST_EMAIL')
+if TEST_EMAIL == "1":
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
 
 WSGI_APPLICATION = 'TheNewSoleProject.wsgi.application'
 
@@ -119,7 +130,7 @@ WSGI_APPLICATION = 'TheNewSoleProject.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ["DATABASE_URL"])
-    }
+}
 
 
 # Password validation
